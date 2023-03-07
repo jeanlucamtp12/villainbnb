fa5141d0f0d63ca6a50872b1178effc633d224e9
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2023-03-07 09:44:40 -0300

    alterações aluguel de bases, adição e listagem

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,31 +6,44 @@ const User = require('../models/user');
 const router = express.Router();
 
 
-
-router.post('/register', async (req, res) => {
-
+const validarDados = (req) =>{
 
     if (req.body.titulo == req.body.nome_fachada) {
-        return res.status(400).send({ error: 'Nome da fachada deve ser diferente do titulo!' });
+        return 'Titulo igual a fachada';
     }
 
     if ((req.body.cidade).toLowerCase() != 'nova york' && (req.body.cidade).toLowerCase() != 'rio de janeiro' && (req.body.cidade).toLowerCase() != 'tóquio') {
-        return res.status(400).send({ error: 'Essa cidade não está no catalogo!' });
+        return 'Cidade indisponivel';
     }
 
     if ((req.body.tecnologias).toLowerCase() != 'laboratório de nanotecnologia' && (req.body.tecnologias).toLowerCase() != 'jardim de ervas venenosas' && (req.body.tecnologias).toLowerCase() != 'estande de tiro' && (req.body.tecnologias).toLowerCase() != 'academia de parkour') {
-        return res.status(400).send({ error: 'Essa tecnologia não está no catalogo!' });
+        return 'Tecnologia indisponivel';
     }
+
+    return 'Já Registrado';
+}
+const getUser = () => undefined;
+
+
+router.post('/register', async (req, res) => {
+
+    var aux = '';
 
     try {
 
+        aux = validarDados(req);
+
+        if (aux != 'Já Registrado'){
+            throw new Error();
+        }
+
         const user = await User.create(req.body);
-
         user.nome_fachada = undefined;
-
         return res.send({ user });
+
     } catch (err) {
-        return res.status(400).send({ error: 'Falhou o registro!' });
+        aux = validarDados(req);
+        return res.status(400).send({ error: 'Falhou o registro! ' + aux});
 
     }
 
@@ -39,33 +52,23 @@ router.post('/register', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
 
-    if (req.body.titulo == req.body.nome_fachada) {
-        return res.status(400).send({ error: 'Nome da fachada deve ser diferente do titulo!' });
+
+    const aux = validarDados(req);
+
+    if (aux != 'Já Registrado') {
+        return res.status(400).send({ error: 'Falha na atualização! ' + aux});
     }
 
-    if ((req.body.cidade).toLowerCase() != 'nova york' && (req.body.cidade).toLowerCase() != 'rio de janeiro' && (req.body.cidade).toLowerCase() != 'tóquio') {
-        return res.status(400).send({ error: 'Essa cidade não está no catalogo!' });
+    if (await User.findOne({ 'titulo': req.body.titulo })) {
+        return res.status(400).send({ error: 'Já existe uma base com esse titulo!'});
     }
 
-    if ((req.body.tecnologias).toLowerCase() != 'laboratório de nanotecnologia' && (req.body.tecnologias).toLowerCase() != 'jardim de ervas venenosas' && (req.body.tecnologias).toLowerCase() != 'estande de tiro' && (req.body.tecnologias).toLowerCase() != 'academia de parkour') {
-        return res.status(400).send({ error: 'Essa tecnologia não está no catalogo!' });
-    }
-
-    const busca = await User.find({titulo: req.body.titulo});
-
-    //if(busca == null){
-    //    return res.status(400).send({ error: 'Esse já o titulo de outra base. Tente novamente!' + busca});
-    //}
 
     const {titulo, nome_fachada, cidade, tecnologias, createdAt} = req.body;
     const {id} = req.params;
-
     const user = await User.findByIdAndUpdate (id, {titulo, nome_fachada, cidade, tecnologias, createdAt}, {new: true});
-
-    return res.status(204).json({user});
-
     
-
+    return res.send({ user });
 });
 
 
@@ -80,16 +83,23 @@ router.get('/listar', async (req, res) => {
         const busca = await User.find().select(['-nome_fachada']);
         return res.json(busca);
     }else{
-        if(titulo){
-            const buscaTitulo = await User.find({'titulo': titulo}).select(['-nome_fachada']);
-            return res.json(buscaTitulo);
-        }else if (cidade){
-            const buscaCidade = await User.find({'cidade': cidade}).select(['-nome_fachada']);
-            return res.json(buscaCidade);
-        }else{
-            const buscaTec = await User.find({'tecnologia': tecnologia}).select(['-nome_fachada']);
-            return res.json(buscaTec);
-        }       
+
+    const buscaTitulo = await User.find({'titulo': titulo}).select(['-nome_fachada']);
+    const buscaCidade = await User.find({'cidade': cidade}).select(['-nome_fachada']);
+    const buscaTec = await User.find({'tecnologia': tecnologia}).select(['-nome_fachada']);
+
+    if(buscaTitulo){
+        return res.json(buscaTitulo);
+    }else if(buscaCidade){
+        return res.json(buscaCidade);
+    }else if (buscaTec){
+        return res.json(buscaTec);
+    }else{
+        return res.status(400).send({ error: 'Nao encontrado!' });
+
+    }
+
+
     }
 
 
@@ -111,13 +121,18 @@ router.post('/:titulo/:nome_fachada', async (req, res) => {
     const {titulo} = req.params;
     const {senha} = req.params;
     const {nome_fachada} = req.params;
-
     const {aluguel} = req.params;
+
 
     const buscaAluguel = await User.findOne({ 'titulo': titulo });
     const buscaAluguelFachada = await User.findOne({ 'nome_fachada': nome_fachada });
 
-    
+    if(req.body.desalugar == true){ 
+        await User.findByIdAndUpdate (buscaAluguel.id, {aluguel: false}, {new: true});
+        return res.status(400).send({ message: 'Aluguel cancelado com sucesso: ' });
+    }
+
+
     if(buscaAluguel.aluguel == true){
         return res.status(400).send({ message: 'A base já está alugada!' });
     }
