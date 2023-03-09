1e41e2c018f78514c5cb32281f3ab328c4c611f6
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2023-03-08 22:44:14 -0300

    correção de listagem e atualização de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
+//configuração para conexão com o banco usando mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', {autoIndex: true});
+mongoose.connect('mongodb://localhost/noderest', {autoIndex: true}, {ignoreUndefined: true});
 
 mongoose.Promise = global.Promise;
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
 
+//definição da aplicação e de porta de conexão 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 
 
 require('./controllers/authController')(app);
